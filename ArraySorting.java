46fce67ac333dca6f2714025daa5048f1f2931d2
amityadav014 <147026487+amityadav014@users.noreply.github.com>
2023-10-28 14:45:32 +0530

    arraysort

--- ArraySorting.java
+++ ArraySorting.java
@@ -2,35 +2,26 @@
 
 public class ArraySorting {
 
-  public static void bubbleSort(int[] array) {
-    int n = array.length;
-    boolean swapped;
-
-    do {
-      swapped = false;
-      for (int i = 1; i < n; i++) {
-        if (array[i - 1] > array[i]) {
-          // Swap elements if they are in the wrong order
-          int temp = array[i - 1];
-          array[i - 1] = array[i];
-          array[i] = temp;
-          swapped = true;
-        }
-      }
-      // After each pass, the largest element is correctly placed at the end, so reduce the array size
-      n--;
-    } while (swapped);
-  }
 
   public static void main(String[] args) {
-    int[] numbers = {5, 2, 9, 1, 5, 6};
 
-    // Sorting the array using Bubble Sort
-    bubbleSort(numbers);
-
-    System.out.println("Sorted Array in Ascending Order:");
-    for (int num : numbers) {
-      System.out.print(num + " ");
+    int[] arr = new int[] {78, 34, 1, 3, 90, 34, -1, -4, 6, 55, 20, -65};
+    System.out.println("Array elements after sorting:");
+//sorting logic
+    for (int i = 0; i < arr.length; i++)
+    {
+      for (int j = i + 1; j < arr.length; j++)
+      {
+        int tmp = 0;
+        if (arr[i] > arr[j])
+        {
+          tmp = arr[i];
+          arr[i] = arr[j];
+          arr[j] = tmp;
+        }
+      }
+//prints the sorted element of the array
+      System.out.println(arr[i]);
     }
   }
 }
